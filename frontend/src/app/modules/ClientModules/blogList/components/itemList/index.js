289f3e86f67617c3c35e12da1ed191ec0d5e9925
rubysky17 <vomanhdat10998@gmail.com>
2022-01-31 14:53:27 +0700

    [M] Modify UI of blog page

--- frontend/src/app/modules/ClientModules/blogList/components/itemList/index.js
+++ frontend/src/app/modules/ClientModules/blogList/components/itemList/index.js
@@ -1,51 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Skeleton from "react-loading-skeleton";
-// import { Link } from "react-router-dom";
-import SecondaryBlog from "../secondaryBlog";
+import LPESecondaryBlog from "../secondaryBlog";
+import LPEPrimaryBlog from "../primaryBlog";
+import LoadingSkeletionBlog from "../skeletonBlog";
 
 function ItemList({ loading, data }) {
-  // const specialBlogItem = (data) => {
-  //   return (
-  //     <div className="col-12 col-md-6 px-1 mb-3 mb-md-0">
-  //       {loading ? (
-  //         <Skeleton width="100%" height={300} />
-  //       ) : (
-  //         <Link to="/">
-  //           <article
-  //             className="lpe-blog big-item"
-  //             style={{
-  //               backgroundImage:
-  //                 "url(" +
-  //                 "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/df36a760735811ec85fc41a2f5147a1f.jpeg" +
-  //                 ")",
-  //             }}
-  //           >
-  //             <div className="wrapper-blog__item">
-  //               <div className="shadow-box">
-  //                 <div className="blog-big_text">
-  //                   <div>
-  //                     <p className="blog-big_text--info">
-  //                       <span>
-  //                         <span className="text-uppercase">
-  //                           Khoa học - Công nghệ
-  //                         </span>{" "}
-  //                         - 12 giờ trước
-  //                       </span>
-  //                     </p>
-  //                     <h2 className="blog-big_text--title">
-  //                       Mọi người cần biết cách đọc số liệu
-  //                     </h2>
-  //                   </div>
-  //                 </div>
-  //               </div>
-  //             </div>
-  //           </article>
-  //         </Link>
-  //       )}
-  //     </div>
-  //   );
-  // };
-
   return (
     <>
       <h1 className="blog-topic">
@@ -53,15 +12,23 @@ function ItemList({ loading, data }) {
       </h1>
 
       <div className="row blog-list">
-        <>
-          {data.map((blog, index) => {
-            return (
-              <Fragment key={index}>
-                {<SecondaryBlog loading={loading} data={blog} />}
-              </Fragment>
-            );
-          })}
-        </>
+        {loading ? (
+          <LoadingSkeletionBlog />
+        ) : (
+          <>
+            {data.map((blog, index) => {
+              return (
+                <>
+                  {index > 1 ? (
+                    <LPESecondaryBlog data={blog} key={index} />
+                  ) : (
+                    <LPEPrimaryBlog data={blog} key={index} />
+                  )}
+                </>
+              );
+            })}
+          </>
+        )}
       </div>
     </>
   );
--- frontend/src/app/modules/ClientModules/blogList/index.js
+++ frontend/src/app/modules/ClientModules/blogList/index.js
@@ -11,10 +11,10 @@ function BlogList() {
   useSiteTitle("blog_page");
 
   const [blogList, setBlogList] = useState([]);
-  const [isLoading, setisLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getBlogList = (page) => {
-    setisLoading(true);
+    setIsLoading(true);
     // const limit = 20;
     // const offset = page === 0 ? limit : (page - 1) * limit;
 
@@ -23,11 +23,11 @@ function BlogList() {
       method: "GET",
     })
       .then((response) => {
-        setisLoading(false);
+        setIsLoading(false);
         setBlogList(response.data.data);
       })
       .catch((error) => {
-        setisLoading(false);
+        setIsLoading(false);
         console.log("error", error);
       });
   };
@@ -38,17 +38,7 @@ function BlogList() {
 
   return (
     <div className="lpe-blog-screen container-fluid container-lg py-5">
-      {/* Fixed category */}
-
-      {/* list blog render with category */}
       <ItemList data={blogList} loading={isLoading} />
-      {/* <LoadingSkeletionBlog
-        loading={!isLoading}
-        count={4}
-        style={{ height: 300, margin: "10px 0px" }}
-      >
-        <ItemList data={DATA_BLOG_SAMPLE} loading={!isLoading} />
-      </LoadingSkeletionBlog> */}
     </div>
   );
 }
